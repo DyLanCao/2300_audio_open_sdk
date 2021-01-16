50ed24aafd49df6295e27cc0b3ea89dfa8600cd1
caoyin <DyLanCao>
2021-01-16 19:13:24 +0800

    add gen lib on i

--- i2s_build.sh
+++
@@ -1,8 +0,0 @@
-make T=i2s_codec DEBUG=1 -j8 > log.txt 2>&1
-
-if [ $? -eq 0 ];then
-	echo "build success"
-else
-	echo "build failed and call log.txt"
-	cat log.txt | grep "error:*"
-fi
--- i2s_clear.sh
+++
@@ -1,1 +0,0 @@
-make T=i2s_codec DEBUG=1 clean
